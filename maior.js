a841ca90ac222330101f29dbd26018c91c8dfb6b
bahdinizo <t.barbara.dutradinizo@gmail.com>
2023-06-15 13:42:13 -0300

    Adição de página que calcula número maior

---
+++ maior.js
@@ -0,0 +1,19 @@
+
+function VerificaMaior(){  
+    var x1 = parseFloat(document.getElementById('nota1').value);
+    var x2 = parseFloat(document.getElementById('nota2').value);
+    var x3 = parseFloat(document.getElementById('nota3').value);
+
+        if (x1 > x2 && x1>x3){
+            alert("O maior número digitado é o:" +x1);
+        }
+        else if (x2 > x1 && x2>x3){
+            alert("O maior número digitado é o:" + x2);
+        }
+        else if (x3 > x2 && x1){
+            alert("O maior número digitado é o:" + x3);
+        }
+        else{
+             alert("Digite 3 números diferentes")
+        }   
+}
